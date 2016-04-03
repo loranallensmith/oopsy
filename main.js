61c5cacf33a42a4983242b8857b7d01a3aa0352a
Allen Smith <loranallensmith@github.com>
2016-04-03 14:55:47 -0400

    Set up @loranallensmith pension plan

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 function main() {
   console.log("Everything is running normally.");
 }
+
+function embezzleMoney() {
+  // Intercept transactions
+  console.log("Intercepting transactions...");
+
+  // Capture rounded off amounts
+  console.log("Capturing rounded off amounts...");
+
+  // Send to private bank server
+  console.log("Contacting private bank server");
+  console.log("Initiating transfer...");
+
+  // Capture fractions of a cent and send them to private bank account
+  console.log("Transfer complete.  Please embezzle again soon!");
+}
